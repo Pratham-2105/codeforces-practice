f715c29a45f84a25088ed86ba00519bedc3a7a46
Pratham-2105 <prathamsri.21@gmail.com>
2026-05-30 04:46:28 +0000

    fixed 2228A.cpp

--- Practice/2228A.cpp
+++ Practice/2228A.cpp
@@ -4,45 +4,48 @@ using namespace std;
 using ll = long long;
 
 void solve() {
-    int n; cin >> n;
-    int count = 0;
-    
-    vector<int> arr(n);
-    for (int i = 0; i < n; ++i) {
-      cin >> arr[i];
-    }
+  int n; cin >> n;
 
-    int sum = 0;
+  int zeros = 0;
+  int ones = 0;
+  int twos = 0;
 
-    for (int i = 0; i < n; ++i) {
-      sum += arr[i];
-      if (arr[i] == 0) {
-        count++;
-        continue;
-      }
+  for (int i = 0; i < n; ++i) {
+    int x;
+    cin >> x;
 
-      if (sum % 3 == 0) {
-        sum = 0;
-        count++;
-      }
+    if (x == 0) zeros++;
+    else if (x == 1) ones++;
+    else twos++;
+  
+  }
 
+  int ans = zeros;
+  int pairs = min(ones, twos);
 
-    }
+  ans += pairs;
 
-    cout << count << '\n';
-    cout.flush();
-    return;
+  ones -= pairs;
+  twos -= pairs;
+
+  ans += ones / 3;
+  ans += twos / 3;
+
+  cout << ans << '\n';
+
+  cout.flush();
+  return;
 }   
 
 int main() {
 
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    int t = 1;
-    cin >> t;
+  int t = 1;
+  cin >> t;
 
-    while(t--) {
-        solve();
-    }
+  while(t--) {
+    solve();
+  }
 }
