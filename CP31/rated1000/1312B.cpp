d34fc782f13a64f04602b47d9cf980bbae3805c7
Pratham-2105 <prathamsri.21@gmail.com>
2026-06-10 17:05:24 +0000

    did this somehow

---
+++ CP31/rated1000/1312B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+
+  sort(arr.rbegin(), arr.rend());
+
+  for (int i = 0; i < n; ++i) {
+    cout << arr[i] << " ";
+  }
+  cout << '\n';
+
+  cout.flush();
+  return;
+}
+
+int main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+}
